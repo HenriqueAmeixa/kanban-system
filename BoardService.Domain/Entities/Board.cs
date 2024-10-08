e7e3c497474bf14a365af0277ced8df2c0c30445
henrique ameixa <henrique-ameixa@hotmail.com>
2024-10-07 22:23:09 -0300

    Enriquecido as classes

--- BoardService.Domain/Entities/Board.cs
+++ BoardService.Domain/Entities/Board.cs
@@ -1,21 +1,21 @@
 ﻿
 namespace BoardService.Domain.Entities
 {
-    public class Board
+    public sealed class Board
     {
-        public int Id { get; set; } 
+        public int Id { get; private set; } 
 
-        public string Name { get; set; }  
+        public string Name { get; private set; }  
 
-        public int OrganizationId { get; set; } 
+        public int OrganizationId { get; private set; } 
 
-        public ICollection<Column> Columns { get; set; } = new List<Column>();  
+        public ICollection<Column> Columns { get; private set; } = new List<Column>();  
 
-        public ICollection<UserBoard> UserBoards { get; set; } = new List<UserBoard>();  
-        public ICollection<KanbanTask> KanbanTasks { get; set; } = new List<KanbanTask>();
+        public ICollection<UserBoard> UserBoards { get; private set; } = new List<UserBoard>();  
+        public ICollection<KanbanTask> KanbanTasks { get; private set; } = new List<KanbanTask>();
 
-        public DateTime CreatedAt { get; set; } 
+        public DateTime CreatedAt { get; private set; } 
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; private set; }
     }
 }
